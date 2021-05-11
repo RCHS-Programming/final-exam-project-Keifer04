4a561b77400e5f325d46ea33fb32023263e0c358
Wishart <keifer.wishart@stu.boone.kyschools.us>
2021-05-11 12:26:40 -0400

    added sprites

---
+++ TeleportOrb.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TeleportOrb here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TeleportOrb extends Actor
+{
+    /**
+     * Act - do whatever the TeleportOrb wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
