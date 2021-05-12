87da653c47765d8d8651ee8d20bf532ad751e2ce
Wishart <keifer.wishart@stu.boone.kyschools.us>
2021-05-12 12:25:38 -0400

    Movement for player

--- Frog.java
+++ Frog.java
@@ -15,5 +15,61 @@ public class Frog extends Actor
     public void act() 
     {
         // Add your action code here.
+        
+        playerMovement();
+        enemyCollision();
     }    
+    
+    public void playerMovement()
+    {
+         if (Greenfoot.isKeyDown("up"))
+        {
+            setLocation( getX(), getY() -3);
+        }   
+        if (Greenfoot.isKeyDown("down"))
+        {
+            setLocation( getX(), getY() +3 );
+        }    
+        if (Greenfoot.isKeyDown("right"))
+        {
+            setLocation( getX() +3, getY() );
+        }    
+        if (Greenfoot.isKeyDown("left"))
+        {
+            setLocation( getX() -3, getY() );
+        }  
+    }
+    
+    public void enemyCollision()
+    {
+     
+        
+    {
+           if (isTouching(demonEye.class))
+           {
+              removeTouching(Frog.class);
+              setLocation( 100, 100);
+            }
+           if (isTouching(Dragon.class))
+           {
+              removeTouching(Frog.class);
+              setLocation(100, 100);
+           }
+           if (isTouching(evilFrog.class))
+           {
+              removeTouching(Frog.class);
+              setLocation(100, 100);
+           }
+           if (isTouching(soundwave.class))
+           {
+              removeTouching(Frog.class);
+              setLocation(100, 100);
+           }
+           if (isTouching(deathOrb.class))
+           {
+              removeTouching(Frog.class);
+              setLocation(100, 100);
+           }
+    }
+    }
 }
--- ground.java
+++ ground.java
@@ -16,6 +16,7 @@ public class ground extends World
     public ground()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(900, 600, 1); 
+        super(900, 600, 1);
+        addObject(new Frog(),100, 100); 
     }
 }
